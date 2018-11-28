b66bb8d6711d8c5a3cd111cf6b2c435de56f8dad
NareshReddy11 <nareshreddy.v1994@gmail.com>
2018-11-28 20:38:16 +0530

    rocketmq

---
+++ app3/src/main/java/naresh/biz/app3/App.java
@@ -0,0 +1,13 @@
+package naresh.biz.app3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ app3/src/main/java/naresh/biz/app3/OrderedProducer.java
@@ -0,0 +1,5 @@
+package naresh.biz.app3;
+
+public class OrderedProducer {
+
+}
